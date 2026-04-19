34444ee5c7b31302c3305d9bad773bf131dabc1b
ignasandriukevicius-pixel <ignas.andriukevicius@gmail.com>
2026-04-19 22:31:40 +0300

    Change redirect path from '/welcome' to '/'

--- src/app/auth/actions.ts
+++ src/app/auth/actions.ts
@@ -20,7 +20,7 @@ export async function login(formData: FormData) {
   }
 
   revalidatePath('/', 'layout')
-  return redirect('/welcome')
+  return redirect('/')
 }
 
 export async function signup(formData: FormData) {
